23b4fb0fc285ee6834d6eca324dc434d1e349ba9
Dylon-Chan <dylon_chan22@hotmail.com>
2023-08-12 16:06:40 +0800

    upload nodejs app

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+
+//constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const OS = require('os');
+const ENV = 'DEV';
+const START = Date.now();
+
+//function
+function elapsed(){
+    const running = (Date.now() - START) / 1000;
+    const minutes = Math.floor(running / 60);
+    const seconds = Math.floor(running % 60);
+    const hours = Math.floor(minutes / 60);
+    return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+//app
+const app = express();
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    const msg = `Hello World from ${OS.hostname()} in ${ENV}! I have been running for ${elapsed()}`;
+    res.send(msg);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
