020f71ffc9d41eefdf5e08a37c77a50cf9762c57
Dylon-Chan <dylon_chan22@hotmail.com>
2023-08-12 16:56:41 +0800

    upload nodejs app

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 const express = require('express');
 
 //constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
 const OS = require('os');
 const ENV = 'DEV';
 const START = Date.now();
@@ -25,8 +23,5 @@ app.get('/', (req, res) => {
     const msg = `Hello World from ${OS.hostname()} in ${ENV}! I have been running for ${elapsed()}`;
     res.send(msg);
 });
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
 
 module.exports = app;
