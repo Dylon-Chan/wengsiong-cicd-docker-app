80dcc2ade7007029f14c51154f8187dcf268f4a4
Dylon-Chan <dylon_chan22@hotmail.com>
2023-08-14 22:13:52 +0800

    .

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "sctp-ce2-tfstate-bkt"
-    key    = "wengsiong-docker-nodejs-app.tfstate"
-    region = "ap-southeast-1"
-  }
-}
--- main.tf
+++
@@ -1,5 +0,0 @@
-#create aws ecr
-resource "aws_ecr_repository" "ecr" {
-  name = "wengsiong-docker-nodejs-app"
-}
-
