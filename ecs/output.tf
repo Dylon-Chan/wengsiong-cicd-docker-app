8d5864d85a64d27a351cb368e535b3e9081ce57f
Dylon-Chan <dylon_chan22@hotmail.com>
2023-08-14 23:12:51 +0800

    update output.tf

--- ecs/output.tf
+++ ecs/output.tf
@@ -4,14 +4,11 @@ data "aws_network_interfaces" "all" {
     name = "group-id"
     values = [aws_security_group.ecs-sg.id]
   }
-  depends_on = [aws_security_group.ecs-sg]
 }
 
 data "aws_network_interface" "all" {
   for_each = toset(data.aws_network_interfaces.all.ids)
     id = each.key
-
-  depends_on = [aws_security_group.ecs-sg , data.aws_network_interfaces.all]
 }
 
 output "all_access_urls" {
